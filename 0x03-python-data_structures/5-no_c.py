625cbd7d0e3468374410146b6b219fa68963bb68
anfenee <mwangokeago@gmail.com>
2022-06-07 11:15:06 +0000

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,10 +4,4 @@ def no_c(my_string):
     for c in my_string:
         if c != 'c' and c != 'C':
             new_string += c
-    return new_string#!/usr/bin/python3
-def no_c(my_string):
-    new_string = ""
-    for c in my_string:
-        if c != 'c' and c != 'C':
-            new_string += c
     return new_string
