25743d6837ef6a1b897dda70b53f5d728e8e30bb
anfenee <mwangokeago@gmail.com>
2022-06-21 19:45:47 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+ng a square and its size"""
+
+
+class Square:
+    """implementing a square"""
+
+    def __init__(self, size=0):
+        """initializing new square with the size argument"""
+        self.__size = size
